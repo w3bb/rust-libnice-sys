b2d6477a17d79d5a1e71f595ec5549a7b968d749
Jonas Herzig <me@johni0702.de>
2019-02-08 12:21:53 +0100

    Generate bindings only for libnice and use glib-sys, etc. otherwise

--- build.rs
+++ build.rs
@@ -11,7 +11,38 @@ fn main() {
         .expect("Failed to find (lib)nice using pkg-config!");
 
     let bindings = bindgen::Builder::default()
-        .header_contents("wrapper.h", "#include <nice/agent.h>")
+        .header_contents(
+            "wrapper.h",
+            "#include <nice/agent.h>
+             #include <nice/interfaces.h>
+
+             #include <stun/stunagent.h>
+             #include <stun/stunmessage.h>
+             #include <stun/constants.h>
+             #include <stun/usages/bind.h>
+             #include <stun/usages/ice.h>
+             #include <stun/usages/turn.h>
+             #include <stun/usages/timer.h>
+
+             #include <nice/pseudotcp.h>",
+        )
+        // ICE Library
+        .whitelist_function("nice_.+")
+        .whitelist_type("NICE.+")
+        .whitelist_type("_?Nice.+")
+        .whitelist_type("_?TurnServer")
+        // STUN Library
+        .whitelist_function("stun_.+")
+        .whitelist_type("STUN.+")
+        .whitelist_type("TURN.+")
+        .whitelist_type("_?[Ss]tun.+")
+        // contains `va_list` type argument which seems like it might not be handled properly
+        .opaque_type("StunDebugHandler")
+        // Pseudo TCP Socket implementation
+        .whitelist_function("pseudo_tcp_.+")
+        .whitelist_type("_?PseudoTcp.+")
+        // Disable recursive whitelisting, we're using libc, glib-sys, etc.
+        .whitelist_recursively(false)
         .clang_args(
             libnice
                 .include_paths
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,20 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use gio_sys::*;
+use gobject_sys::*;
+use glib_sys::*;
+use libc::*;
+
+type gsize = usize;
+type gssize = isize;
+type guint64 = u64;
+type guint32 = u32;
+type guint16 = u16;
+type guint8 = u8;
+type guint = c_uint;
+type gint = c_int;
+type guchar = c_uchar;
+type gchar = c_char;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
